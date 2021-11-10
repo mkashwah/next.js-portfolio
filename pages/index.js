99d0a39ce110245c2eb07e3bc02aca68e2e79349
M. Kashwah <mohammed.kashwah@gmail.com>
2021-11-10 04:38:08 -0500

    start Next.js project

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+//express server setup
+// const express = require('express')
+// const app = express()
+// const port = 3001
+
+// app.get('/',(req,res) =>{
+//     res.send('server is running')
+// })
+
+// app.listen(port,()=>{
+//     console.log(`app listening at localhost:${port}`)
+// })
